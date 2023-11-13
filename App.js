017b970a5bc4414689bafb1b51485d9704bd5861
Tuomasromppanen <tuomas.romppanen@outlook.com>
2023-11-13 10:25:09 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
+import Home from './components/Home';
+import Loading from './components/Loading';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [isloading, setIsLoading] = useState(true);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    }, 2000)
+  })
+
+  if (isloading) {
+    return (
+      <Loading />
+    )
+  } 
+  else {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          sceneContainerStyle={{ backgroundColor: 'transparent' }}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName
+              if (route.name === 'Home') {
+                iconName = focused
+                  ? 'home'
+                  : 'home-outline'
+              } else if (route.name === 'Gameboard') {
+                iconName = focused
+                  ? 'dice-multiple'
+                  : 'dice-multiple-outline'
+              } else if (route.name === 'Scoreboard') {
+                iconName = focused
+                  ? 'view-list'
+                  : 'view-list-outline'
+              }
+              return <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            },
+            tabBarActiveTintColor: 'black',
+            tabBarInactiveTintColor: 'gray'
+          })}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+          />
+        </Tab.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+
+    );
+  }
+}
---
+++ components/Loading.js
@@ -0,0 +1,15 @@
+import { Image, Text, View } from "react-native";
+import logo from '../logo/Expo.png'
+import styles from '../style/styles'
+
+export default function Loading() {
+    return (
+    <View style={styles.container}>
+        <Image 
+            style={styles.logo}
+            source={logo}
+        />
+        <Text>Loading...</Text>
+    </View>
+    )
+}
