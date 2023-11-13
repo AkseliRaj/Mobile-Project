c12077c741f863b8cc901c2a477c5d93ebf09276
Tuomasromppanen <tuomas.romppanen@outlook.com>
2023-11-13 19:21:56 +0200

    Initial commit

--- components/Home.js
+++ components/Home.js
@@ -1,10 +1,19 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import logo from '../assets/expo.png'
 import styles from '../style/styles'
 
 export default function Home() {
     return (
-        <View style={styles.container}>
-            <Text>Welcome to home page</Text>
+        <View>
+            <Image
+                style={styles.logo}
+                source={logo}
+            />
+            <Text>Welcome to Cryptex</Text>
+            <Text>Stay up-to-date with the latest market trends and explore a wide range of currencies.</Text>
+            <Text>Make planning for your next investment convenient with the intuitive currency converter.</Text>
+            <Text>Your trading journey starts here.</Text>
+            <Text>Stocks, with the biggest price:</Text>
         </View>
     )
 }
