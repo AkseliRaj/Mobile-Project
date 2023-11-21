25742c53a7c9f98e5657ea460e27526ab8646eee
Miikakyl <kylmanen.miika@gmail.com>
2023-11-21 17:32:49 +0200

    Fixed problem where Conversion results are't showed entirely if the amount of digits is large

--- components/ConversionResult.js
+++ components/ConversionResult.js
@@ -1,12 +1,26 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ConversionResult = ({ isSwapped, isConverted, amount, crypto, currency, result }) => {
+    const [flexDirection, setFlexDirection] = useState("row")
 
 
+    // Changing the direction of flex from row to column if the amount of digits are over 20
+    
+    useEffect(() => {
+
+        const amountOfDigits = Math.abs(result).toString() + Math.abs(amount).toString()
+        console.log(amountOfDigits.length)
+        setFlexDirection(
+            amountOfDigits.length > 20
+                ? "column"
+                : "row"
+        )
+    }, [result])
+    
     return (
         <>{isConverted &&
-            <View style={styles.resultContainer}>
+            <View style={[styles.resultContainer, {flexDirection: flexDirection}]}>
                 {isSwapped
                     ? <Text style={{ fontSize: 18 }}>{amount} {currency}</Text>
                     : <Text style={{ fontSize: 18 }}>{amount} {crypto}</Text>
@@ -23,7 +37,6 @@ const ConversionResult = ({ isSwapped, isConverted, amount, crypto, currency, re
 const styles = StyleSheet.create({
     resultContainer: {
         marginTop: 20,
-        flexDirection: "row",
         gap: 10,
         alignItems: "center",
         justifyContent: "center",
