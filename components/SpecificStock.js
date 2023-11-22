11ff650bfa443c95c07bc6d7f48558e192fcd3aa
AkseliRaj <akseli.rajahalme@gmail.com>
2023-11-22 14:18:03 +0200

    Added styling to SpecificStock

--- components/SpecificStock.js
+++ components/SpecificStock.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { View, Text, Pressable, Dimensions } from 'react-native';
+import { View, Text, Pressable, Dimensions, StyleSheet } from 'react-native';
 import { Image as ExpoImage } from 'expo-image';
-import styles from '../style/styles';
 import { LineChart } from "react-native-chart-kit";
 import { useEffect, useState } from 'react';
 import { getCoinDetails } from '../api/Functions';
@@ -42,77 +41,137 @@ const SpecificStock = ({ navigation, route }) => {
 
   return (
     <View style={styles.container}>
-      
-      <LineChart
-        data={{
-          labels: ["January", "February", "March", "April", "May", "June"],
-          datasets: [
-            {
-              data: [
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100,
-                Math.random() * 100
-              ]
+      <View style={styles.chartContainer}>
+        <LineChart
+          data={{
+            labels: ["January", "February", "March", "April", "May", "June"],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100
+                ]
+              }
+            ]
+          }}
+          width={Dimensions.get("window").width - 40} // from react-native
+          height={250}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#ffffff",
+            backgroundGradientFrom: "#ffffff",
+            backgroundGradientTo: "#ffffff",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(0, 76, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(109, 109, 109, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#004CFF"
             }
-          ]
-        }}
-        width={Dimensions.get("window").width - 40} // from react-native
-        height={250}
-        yAxisLabel="$"
-        yAxisSuffix="k"
-        yAxisInterval={1} // optional, defaults to 1
-        chartConfig={{
-          backgroundColor: "#ffffff",
-          backgroundGradientFrom: "#ffffff",
-          backgroundGradientTo: "#ffffff",
-          decimalPlaces: 2, // optional, defaults to 2dp
-          color: (opacity = 1) => `rgba(0, 76, 255, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(109, 109, 109, ${opacity})`,
-          style: {
-            borderRadius: 10
-          },
-          propsForDots: {
-            r: "6",
-            strokeWidth: "2",
-            stroke: "#004CFF"
-          }
-        }}
-        
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-      />
-      <View>
-        <Text>{coin.name}</Text>
+          }}
+          
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
       </View>
       <View>
-        <View>
-          <Text>Price</Text>
-          <Text>{parseFloat(coin.price).toFixed(2)} $</Text>
+        <View style={styles.coinNameHeadingContainer}>
+          <Text style={styles.coinNameHeading}>{coin.name}</Text>
         </View>
         <View>
-          <Text>Volume 24h</Text>
-          <Text>{coin['24hVolume']} $</Text>
-        </View>
-        <View>
-          <Text>Circulating supply</Text>
-          <Text>{coin.supply.circulating} BTC</Text>
-        </View>
-        <View>
-          <Text>Market Cap</Text>
-          <Text>{coin.marketCap} $</Text>
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataHeading}>Price</Text>
+            <Text style={styles.dataText}>{parseFloat(coin.price).toFixed(2)} $</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataHeading}>Volume 24h</Text>
+            <Text style={styles.dataText}>{coin['24hVolume']} $</Text>
+          </View>
+          <View style={styles.dataContainer}>
+            <Text style={styles.dataHeading}>Circulating supply</Text>
+            <Text style={styles.dataText}>{coin.supply.circulating} BTC</Text>
+          </View>
+          <View>
+            <Text style={styles.dataHeading}>Market Cap</Text>
+            <Text style={styles.dataText}>{coin.marketCap} $</Text>
+          </View>
         </View>
       </View>
+      
 
-      <Pressable onPress={navigateToCurrencyConverter}>
-        <Text>Go to Currency Converter page</Text>
+      <Pressable style={styles.SpecificStockButton} onPress={navigateToCurrencyConverter}>
+        <Text style={styles.SpecificStockButtonText}>Go to Currency Converter page</Text>
       </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+
+  dataContainer: {
+    marginBottom: 13,
+  },
+  
+  coinNameHeadingContainer: {
+    marginBottom: 25,
+    marginTop: 25,
+  },
+
+  chartContainer: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 16,
+    overflow: 'hidden', // This is important to prevent overflow of border
+    padding: 5,
+  },
+
+  coinNameHeading: {
+    fontWeight: "bold",
+    fontSize: 26,
+  },  
+
+  dataHeading: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  dataText: {
+    fontSize: 14,
+  },
+
+  SpecificStockButton: {
+    backgroundColor: '#004CFF',
+    borderRadius: 14,
+    padding: 13,
+    paddingHorizontal: 25,
+    marginTop: 30,
+  },
+
+  SpecificStockButtonText: {
+    fontSize: 15,
+    textAlign: "center",
+    color: "white",
+  },
+
+})
 
 export default SpecificStock;
