b56f7c243a00c7351dea68c9946f44ac72c24197
TKLahtinen <n2lati00@students.oamk.fi>
2023-12-18 10:04:57 +0200

    Add Dark Mode support to ConversionResult component

--- components/ConversionResult.js
+++ components/ConversionResult.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { converterDM } from '../style/stylesDM'
+import { converter } from '../style/styles'
+import DarkModeContext from './DarkModeContext'
 
 const ConversionResult = ({ isSwapped, isConverted, amount, crypto, currency, result }) => {
     const [flexDirection, setFlexDirection] = useState("row")
+    const { darkModeSet } = useContext(DarkModeContext);
 
-    const data = converterDM
+    const data = darkModeSet ? converterDM : converter
     
     // Changing the direction of flex from row to column if the amount of digits are over 20
     
